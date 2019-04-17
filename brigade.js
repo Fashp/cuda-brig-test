310439de8d6be1636bc0293f40bfcf1c372ae7b8
Eric Zimmer <ezimmer@barraucda.com>
2019-04-17 16:37:32 -0400

    fixed stuff

--- brigade.js
+++ brigade.js
@@ -1,6 +1,6 @@
 const { events, Job } = require("brigadier");
 events.on("simpleevent", () => {
-  var job = new Job("hello world", "alpine:3.8");
+  var job = new Job("hello-world", "alpine:3.8");
   job.tasks = [
     "echo Hello",
     "echo World"
