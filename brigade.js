4172d8747cf7297445a002d85ad7fc39dbe75071
Eric Zimmer <ezimmer@barraucda.com>
2019-04-18 15:00:13 -0400

    ff2

--- brigade.js
+++ brigade.js
@@ -2,7 +2,7 @@ const { events, Job } = require("brigadier");
 events.on("simpleevent", (e, p) => {
   var job = new Job("hello-world", "alpine:3.8"); // "packages.bco.cudaops.com/ccb-docker-local/transfer-engine:latest");
   job.tasks = [
-    "echo Hello " + e.type,
+    "echo Hello " + e.data.f,
     "echo World" + p.name
   ];
 
