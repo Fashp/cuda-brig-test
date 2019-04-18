e26797542cf1ec2eceeecfc67a6ffae8b38d94bb
Eric Zimmer <ezimmer@barraucda.com>
2019-04-18 11:32:47 -0400

    transfer engine time?

--- brigade.js
+++ brigade.js
@@ -1,10 +1,6 @@
 const { events, Job } = require("brigadier");
 events.on("simpleevent", () => {
-  var job = new Job("hello-world", "alpine:3.8");
-  job.tasks = [
-    "echo Hello",
-    "echo World"
-  ];
+  var job = new Job("hello-world", "packages.bco.cudaops.com/ccb-docker-local/transfer-engine:latest");
 
   job.run();
 });
