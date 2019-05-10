bd35b1c0bdb0c28ec26ad92eda0ff83248014d77
Eric Zimmer <ezimmer@barraucda.com>
2019-05-10 10:12:20 -0400

    fff

--- brigade.js
+++ brigade.js
@@ -3,9 +3,13 @@ events.on("simpleevent", (e, p) => {
   var job = new Job("transfer-engine", "packages.bco.cudaops.com/ccb-docker-local/transfer_engine:latest");
   env_vars = JSON.parse(e.payload)
 
+  job.tasks = [
+    "echo Hello"
+  ];
+
   job.env = {
-  	"CUDA_MANIFEST_STORE_ADDRESS": env_vars.manifest_address,
-	"CUDA_DATASOURCE_ADDRESS": env_vars.datasource_address,
+  	"CUDA_MANIFEST_STORE_ADDRESS": env_vars.manifest_store_address,
+	"CUDA_DATA_SOURCE_ADDRESS": env_vars.data_source_address,
 	"CUDA_MANIFEST_ID": env_vars.manifest_id,
 	"CUDA_PATH": env_vars.path
   }
