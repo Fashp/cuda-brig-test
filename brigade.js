e2b48503684ff2bd16fa318f4c4bc5136900dcda
Eric Zimmer <ezimmer@barraucda.com>
2019-05-10 10:16:08 -0400

    ffff

--- brigade.js
+++ brigade.js
@@ -3,10 +3,6 @@ events.on("simpleevent", (e, p) => {
   var job = new Job("transfer-engine", "packages.bco.cudaops.com/ccb-docker-local/transfer_engine:latest");
   env_vars = JSON.parse(e.payload)
 
-  job.tasks = [
-    "echo Hello"
-  ];
-
   job.env = {
   	"CUDA_MANIFEST_STORE_ADDRESS": env_vars.manifest_store_address,
 	"CUDA_DATA_SOURCE_ADDRESS": env_vars.data_source_address,
