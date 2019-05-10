a0b482da71a3094e455ecab1295fc45cd4b7195f
Eric Zimmer <ezimmer@barraucda.com>
2019-05-10 08:40:43 -0400

    did it

--- brigade.js
+++ brigade.js
@@ -1,6 +1,6 @@
 const { events, Job } = require("brigadier");
 events.on("simpleevent", (e, p) => {
-  var job = new Job("transfer-engine", "packages.bco.cudaops.com/ccb-docker-local/transfer-engine:latest");
+  var job = new Job("transfer-engine", "packages.bco.cudaops.com/ccb-docker-local/transfer_engine:latest");
   env_vars = JSON.parse(e.payload)
 
   job.env = {
